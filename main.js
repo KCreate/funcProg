50b6dd83204fa60db52159d6e9cf256b4119e3ca
mendurim <mendurim.r@gmail.com>
2016-04-12 21:12:34 +0200

    fin push

--- main.js
+++ main.js
@@ -2,4 +2,14 @@ function funcProg(collection, source){
 	var newArr =[]; 
 
 	var sourceKey = Object.keys(source); 
+
+	newArr= collection.filter(function(obj){
+		return sourceKey.every(function(value){
+			return obj.hasOwnProperty(value) && obj[value] == source[value]; 
+		}); 
+	}); 
+	return newArr; 
 }
+
+// call 
+funcProg([{ first: "Neron", last: "Hadid" }, { first: "Mars", last: null }, { first: "Tyrell", last: "Lanister" }], { last: "Lanister" });
